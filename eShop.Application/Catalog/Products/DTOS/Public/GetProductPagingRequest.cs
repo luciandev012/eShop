d4754fb2d907204c630ee1aedd930da6f4af72ec
thang1224 <thanguk04@gmail.com>
2021-04-28 23:40:17 +0700

    create method for manage

---
+++ eShop.Application/Catalog/Products/DTOS/Public/GetProductPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using eShop.Application.Catalog.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Application.Catalog.Products.DTOS.Public
+{
+    public class GetProductPagingRequest : PagingRequestBase
+    {
+        public int CategoryId { get; set; }
+    }
+}
---
+++ eShop.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,14 @@
+﻿using eShop.Application.Catalog.DTOS;
+using eShop.Application.Catalog.Products.DTOS;
+using eShop.Application.Catalog.Products.DTOS.Public;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        PagedViewModel<ProductViewModel> GetAllByCategoryId(GetProductPagingRequest request);
+    }
+}
